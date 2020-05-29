15e5a5698404c3de963116dd0f2e6ff0066d71e1
michelXCVI <mcharl05@nyit.edu>
2020-05-29 18:06:17 -0400

    endpoints

---
+++ src/models/ers_reimbursement_type-model.ts
@@ -0,0 +1,22 @@
+export class ErsReimbType{
+    id: number;
+    type: string;
+    
+static from (obj: ErsReimbTypeRow): ErsReimbType{
+    const ersReimbType = new ErsReimbType(
+        obj.reimb_type_id, obj.reimb_type
+    );
+    return ersReimbType;
+} 
+
+constructor(id: number, type: string){
+    this.id = id;
+    this.type = type;
+}
+}
+
+
+export interface ErsReimbTypeRow{
+    reimb_type_id: number;
+    reimb_type: string
+}
