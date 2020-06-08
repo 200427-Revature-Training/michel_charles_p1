af4d26b447956d17da55a4967a8c8b3d07e94dc2
michelXCVI <mcharl05@nyit.edu>
2020-06-08 09:43:55 -0400

    latest updates

---
+++ front-end/src/components/main.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Route , Switch} from 'react-router-dom';
+import{NavComponent} from './nav.component';
+
+
+export const MainComponent: React.FC =() =>{
+return <div>
+
+        <NavComponent></NavComponent>
+        </div>
+}
+
---
+++ front-end/src/components/nav.component.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
+
+export const NavComponent: React.FC = () => {
+
+const classes = useStyles();
+
+return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            ERS System
+          </Typography>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Create an account</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+
